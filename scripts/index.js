3fc3774b68134a25febb085674d667cc9182a455
Anna Koziol <aniakoz99@gmail.com>
2020-06-24 12:43:05 +0200

    Fix RWD bugs, add "skills" section and change texts

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     $(document).ready(function() {
-        console.log("xx")
         new WOW().init();
 
         var selectedClass = "";
