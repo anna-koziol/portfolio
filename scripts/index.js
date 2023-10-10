3d89914eab3a2dcffbd5d1c3fc66632b3ecd5b7c
Anna Koziol <aniakoz99@gmail.com>
2023-10-10 20:02:08 +0200

    update data for QA, add tester projects section

--- scripts/index.js
+++ scripts/index.js
@@ -1,70 +1,71 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    $(document).ready(function() {
-        new WOW().init();
+document.addEventListener("DOMContentLoaded", function (event) {
+  $(document).ready(function () {
+    new WOW().init();
 
-        var selectedClass = "";
-        $(".filter").click(function() {
-            selectedClass = $(this).attr("data-rel");
-            $("#gallery").fadeTo(100, 0.1);
-            $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-            setTimeout(function() {
-                $("." + selectedClass).fadeIn().addClass('animation');
-                $("#gallery").fadeTo(300, 1);
-            }, 300);
-        });
+    var selectedClass = "";
+    $(".filter").click(function () {
+      selectedClass = $(this).attr("data-rel");
+      $("#gallery").fadeTo(100, 0.1);
+      $("#gallery div")
+        .not("." + selectedClass)
+        .fadeOut()
+        .removeClass("animation");
+      setTimeout(function () {
+        $("." + selectedClass)
+          .fadeIn()
+          .addClass("animation");
+        $("#gallery").fadeTo(300, 1);
+      }, 300);
+    });
 
-        $(".tech-icons div")
-            .mouseover(function() {
-                //get p selector with description
-                let p = this.children[1];
-                $(p).css("opacity", 1);
-                $(p).addClass("icons-mouseover");
-                //get i selector 
-                let i = this.children[0];
-                $(i).css("color", "rgba(248, 0, 62, 0.4)");
-                $(i).css("transition", "1s");
+    $(".tech-icons div")
+      .mouseover(function () {
+        //get p selector with description
+        // let p = this.children[1];
+        // $(p).css("opacity", 1);
+        // $(p).addClass("icons-mouseover");
+        //get i selector
+        let i = this.children[0];
+        $(i).css("color", "rgba(248, 0, 62, 0.4)");
+        $(i).css("transition", "1s");
+      })
+      .mouseout(function () {
+        // let p = this.children[1];
+        // $(p).css("opacity", 0);
+        // $(p).removeClass("icons-mouseover");
+        let i = this.children[0];
+        $(i).css("color", "#212529");
+      });
 
-            })
-            .mouseout(function() {
-                let p = this.children[1];
-                $(p).css("opacity", 0);
-                $(p).removeClass("icons-mouseover");
-                let i = this.children[0];
-                $(i).css("color", "#212529");
-            });
+    //generate elements in section Portfolio
+    $.getJSON("./scripts/portfolio.json", function (json) {
+      generatePortfolioTab(Object.values(json));
+      generatePortfolioModal(Object.values(json));
+    });
 
+    //show only web/mobile projects
+    $("#portfolio-all").click(function () {
+      $(".card").hide();
+      $(".cardsContainer-web").show();
+      $(".cardsContainer-mobile").show();
+    });
 
-        //generate elements in section Portfolio
-        $.getJSON("./scripts/portfolio.json", function(json) {
-            generatePortfolioTab(Object.values(json));
-            generatePortfolioModal(Object.values(json));
-        });
+    $("#portfolio-web").click(function () {
+      $(".card").hide();
+      $(".cardsContainer-web").show();
+    });
 
-        //show only web/mobile projects
-        $("#portfolio-all").click(function() {
-            $(".card").hide();
-            $(".cardsContainer-web").show();
-            $(".cardsContainer-mobile").show();
-        });
-
-        $("#portfolio-web").click(function() {
-            $(".card").hide();
-            $(".cardsContainer-web").show();
-        });
-
-        $("#portfolio-mobile").click(function() {
-            $(".card").hide();
-            $(".cardsContainer-mobile").show();
-        });
-
-
-    })
-})
+    $("#portfolio-mobile").click(function () {
+      $(".card").hide();
+      $(".cardsContainer-mobile").show();
+    });
+  });
+});
 
 const generatePortfolioTab = (data) => {
-    data.map((portfolioObjects) => {
-        let cardsContainer = $('#cardsContainer');
-        cardsContainer.append(` 
+  data.map((portfolioObjects) => {
+    let cardsContainer = $("#cardsContainer");
+    cardsContainer.append(` 
         <div class="col-md-12 col-lg-4">
             <a class='card hoverable mb-4 cardsContainer-${portfolioObjects.type}' data-toggle='modal' data-target='#modal-${portfolioObjects.id}' id='${portfolioObjects.id}'> 
                 <img class = 'card-img-top' src = '${portfolioObjects.previewImgSrc}' alt = '${portfolioObjects.name} page image' >
@@ -73,15 +74,15 @@ const generatePortfolioTab = (data) => {
                     <p class = 'card-text text-uppercase mb-3' > ${portfolioObjects.shortDescription} </p>
                 </div>
             </a>
-        </div>`)
-    })
+        </div>`);
+  });
 };
 
 const generatePortfolioModal = (data) => {
-    data.map((portfolioObjects) => {
-        let modalsContainer = $('#modalsContainer');
+  data.map((portfolioObjects) => {
+    let modalsContainer = $("#modalsContainer");
 
-        modalsContainer.append(` 
+    modalsContainer.append(` 
         <div class="modal fade" id="modal-${portfolioObjects.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
             <div class="modal-content">
@@ -126,6 +127,6 @@ const generatePortfolioModal = (data) => {
                 </div>
             </div>
         </div>
-    </div>`)
-    })
+    </div>`);
+  });
 };
